7f7c0f41314e8b6841d57163e9050bd66e063e48
satooru65536 <ryuto4619@gmail.com>
2024-11-17 00:52:03 +0900

    feat: 自動回転

--- genshin-dice/script.js
+++ genshin-dice/script.js
@@ -1,27 +1,11 @@
 const dice = document.getElementById('🎲');
-const xInput = document.getElementById('x');
-const yInput = document.getElementById('y');
-const zInput = document.getElementById('z');
 
-function update() {
-  const x = xInput.value;
-  const y = yInput.value;
-  const z = zInput.value;
-
+function setTransform(x, y, z) {
   dice.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
-
-  localStorage.setItem('x', x);
-  localStorage.setItem('y', y);
-  localStorage.setItem('z', z);
 }
 
-xInput.addEventListener('input', update);
-yInput.addEventListener('input', update);
-zInput.addEventListener('input', update);
-
-document.addEventListener('DOMContentLoaded', () => {
-  xInput.value = localStorage.getItem('x') || 0;
-  yInput.value = localStorage.getItem('y') || 0;
-  zInput.value = localStorage.getItem('z') || 0;
-  update();
-});
+let t = 0;
+setInterval(() => {
+  t += 0.1;
+  setTransform(t, t * 10, 0);
+}, 10);
